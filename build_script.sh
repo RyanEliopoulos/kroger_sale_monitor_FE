a2904dfca81da4d81084771a15118e8de0b5c4d9
Ryan <ryan.eliopoulos@gmail.com>
2022-11-17 11:29:01 -0800

    fixed build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -1,4 +1,5 @@
-git pull                                                                                                                                                                                                                            2 cd ./src
+git pull                                                                                                                                                                                                                                2 cd ./src                                                                                                                                                                                                                        2 grep -rl localhost . | xargs sed -i 's%http://localhost:[0-9]*/%/api/sale_monitor/%'
+cd ./src
 grep -rl localhost . | xargs sed -i 's%http://localhost:[0-9]*/%/api/sale_monitor/%'
 npm install
 npm run build
