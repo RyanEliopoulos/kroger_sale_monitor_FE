6196f4b794163dd5cc819c3cf017d4903863c64e
Ryan <ryan.eliopoulos@gmail.com>
2022-11-17 11:26:11 -0800

    +deploy build script

---
+++ build_script.sh
@@ -0,0 +1,4 @@
+git pull                                                                                                                                                                                                                            2 cd ./src
+grep -rl localhost . | xargs sed -i 's%http://localhost:[0-9]*/%/api/sale_monitor/%'
+npm install
+npm run build
